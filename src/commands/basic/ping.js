a12e61c50f921fc2b96aeb16f7510b0970e1c3a9
DerpyTheDev <95383180+DerpyTheDev@users.noreply.github.com>
2022-03-30 21:18:25 +0200

    Update ping.js

--- src/commands/basic/ping.js
+++ src/commands/basic/ping.js
@@ -1,1 +1,28 @@
 
+const Discord = require('discord.js');
+const {
+	SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+	MessageEmbed
+} = require('discord.js');
+const {
+	footer,
+	normal,
+	error
+} = require('../../config/embed.json');
+const {
+	MessageButton,
+	MessageActionRow
+} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("")
+		.setDescription(""),
+	async execute(interaction, client) {
+           interaction.reply({
+            content: 'pong'
+            });
+	},
+};
